92e511638630eaabdf7930316a47c5be1304e4a6
Dev Patel <dvptl68@gmail.com>
2020-07-29 08:32:03 -0400

    Add data generators

--- imageParser.py
+++ imageParser.py
@@ -9,11 +9,18 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # Retrieve dataset
 path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin='https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip', extract=True)
 
-# Delete created zip file
-os.remove(path_to_zip)
-
 # Set dataset paths
 train_cats_dir = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered', 'train', 'cats')
 train_dogs_dir = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered', 'train', 'dogs')
 validation_cats_dir = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered', 'train', 'cats')
 validation_dogs_dir = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered', 'train', 'dogs')
+
+# Useful variables
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+# Training and validation data generator
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
