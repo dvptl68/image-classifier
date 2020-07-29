76d1f11ed640c70b6b9ec2c4fa6fdf1572aa46c4
Dev Patel <dvptl68@gmail.com>
2020-07-29 08:00:28 -0400

    Install major dependencies

--- imageParser.py
+++ imageParser.py
@@ -1,2 +1,9 @@
 from keras.datasets import mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+import tensorflow as tensor
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.models import model_from_json
+from keras.models import load_model
+import matplotlib.pyplot as plt
+
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
