d6eb99a76f2882b1292590e330673b1a37936248
Dev Patel <dvptl68@gmail.com>
2020-08-01 10:41:27 -0400

    Set dataset paths

--- classifier.py
+++ classifier.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Set dataset paths
+# Dataset paths
 train_dir = os.path.join('datasets', 'cats_and_dogs_filtered', 'train')
 validation_dir = os.path.join('datasets', 'cats_and_dogs_filtered', 'validation')
 train_cats_dir = os.path.join(train_dir, 'cats')
--- classifier2.py
+++ classifier2.py
@@ -3,3 +3,27 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+import os
+
+# Dataset paths
+train_dir = os.path.join('datasets', 'cats_and_dogs_filtered', 'train')
+validation_dir = os.path.join('datasets', 'cats_and_dogs_filtered', 'validation')
+train_cats_dir = os.path.join(train_dir, 'cats')
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+# Useful variables
+num_cats_tr = len(os.listdir(train_cats_dir))
+num_dogs_tr = len(os.listdir(train_dogs_dir))
+
+num_cats_val = len(os.listdir(validation_cats_dir))
+num_dogs_val = len(os.listdir(validation_dogs_dir))
+
+total_train = num_cats_tr + num_dogs_tr
+total_val = num_cats_val + num_dogs_val
+
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
